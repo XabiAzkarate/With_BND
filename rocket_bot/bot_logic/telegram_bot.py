dc9a7d8ed95012efdc1dc1090ba87954c217bb6c
XabiAzkarate <xabier_azkarate@hotmail.com>
2023-09-27 15:42:29 +0200

    Full functionality

--- rocket_bot/bot_logic/telegram_bot.py
+++ rocket_bot/bot_logic/telegram_bot.py
@@ -1,51 +1,66 @@
-import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, CallbackContext, Updater
+from telegram.ext import CommandHandler, CallbackContext, Updater, CallbackQueryHandler
 from . import BisectionSearch
+from decouple import config
 
-TOKEN = os.environ.get('XABIER_ROCKET_BOT_API_TOKEN')
-VIDEO_NAME = os.environ.get('VIDEO_NAME')
+TOKEN = config('XABIER_ROCKET_BOT_API_TOKEN')
+VIDEO_NAME = config('VIDEO_NAME')
 
-search = BisectionSearch(VIDEO_NAME)
+bisection_logic = BisectionSearch(VIDEO_NAME)
 
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     update.message.reply_text(f'Hi {user.first_name}! Ready to find the rocket launch frame?')
 
-    # Start the bisection process
-    frame = search.get_mid_frame()
-    frame_data = search.retrieve_frame(frame)
+    frame = bisection_logic.get_mid_frame()
+    frame_url = bisection_logic.retrieve_frame(frame)
 
-    # You can now send the frame_data to the user and ask them if the rocket has launched.
+    ask_user_about_frame(update, frame_url)
 
-def main() -> None:
-    updater = Updater(token=TOKEN)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
+def ask_user_about_frame(update: Update, frame_url: str):
 
-    # Add more handlers to process user's feedback about the rocket launch
-    updater.start_polling()
-    updater.idle()
+    # Check if this is a callback or a new message
+    if update.callback_query:
+        message = update.callback_query.message
+    else:
+        message = update.message
 
-if __name__ == '__main__':
-    main()
-
-def ask_user_about_frame(update: Update, frame_data: dict):
-    # Show the frame to the user (however you've planned, maybe as an image or a link)
+    message.reply_photo(photo=frame_url, caption="Examine the frame above.")
+    
     keyboard = [
         [InlineKeyboardButton("Rocket has launched", callback_data='launched'),
          InlineKeyboardButton("Rocket has not launched", callback_data='not_launched')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Has the rocket launched in this frame?', reply_markup=reply_markup)
+    message.reply_text('Has the rocket launched in this frame?', reply_markup=reply_markup)
 
 def handle_user_feedback(update: Update, context: CallbackContext):
     query = update.callback_query
-    query.answer()  # End the loading state of the button
+    query.answer()
+
     if query.data == 'launched':
-        search.update_bounds(True)
+        bisection_logic.update_bounds(True)
     else:
-        search.update_bounds(False)
-    next_frame = search.get_mid_frame()
-    next_frame_data = search.retrieve_frame(next_frame)
-    ask_user_about_frame(update, next_frame_data)
+        bisection_logic.update_bounds(False)
+
+    if bisection_logic.low == bisection_logic.high:
+        query.message.reply_text(f"The rocket launch frame is identified as frame number {bisection_logic.low}.")
+    else:
+        next_frame = bisection_logic.get_mid_frame()
+        next_frame_data = bisection_logic.retrieve_frame(next_frame)
+        ask_user_about_frame(update, next_frame_data)
+
+def main() -> None:
+    if not TOKEN:
+        raise ValueError("Telegram bot token is not set!")
+
+    updater = Updater(token=TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CallbackQueryHandler(handle_user_feedback))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
--- rocket_bot/frame_x_integration/frame_controller.py
+++ rocket_bot/frame_x_integration/frame_controller.py
@@ -1,11 +1,16 @@
-from .frame_api_client import FrameXApiClient
+from urllib.parse import urljoin
 
 
 class FrameXController():
 
-    def __init__(self, video_name: str):
-        self._api_client = FrameXApiClient(video_name)
+    BASE_URL = 'http://framex-dev.wadrid.net/api/'
+    GET_FRAME_ENDPOINT = 'video/{video_name}/frame/{frame_number}/'
 
-    def get_frame(self, frame):
-        return self._api_client.get_frame(frame)
+    def __init__(self, video_name: str):
+        self._video_name = video_name
+
+    def get_frame(self, frame_number):
+        response_data =  urljoin(self.BASE_URL, self.GET_FRAME_ENDPOINT.format(video_name=self._video_name, frame_number=frame_number))
+        print(response_data)
+        return response_data
     
